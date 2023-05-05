b74003e7573913ead3965e20bcfb00438e89f009
Eland Woo <gongzi_1076@163.com>
2023-05-05 17:06:41 +0800

    Craft a chatroom.

--- go.mod
+++ go.mod
@@ -3,14 +3,22 @@ module gin_study
 go 1.19
 
 require (
+	github.com/gin-gonic/gin v1.9.0
+	github.com/gorilla/websocket v1.5.0
+)
+
+require (
 	github.com/bytedance/sonic v1.8.8 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
+	github.com/gin-contrib/sessions v0.0.5 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
-	github.com/gin-gonic/gin v1.9.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.13.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/gorilla/context v1.1.1 // indirect
+	github.com/gorilla/securecookie v1.1.1 // indirect
+	github.com/gorilla/sessions v1.2.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
@@ -23,7 +31,7 @@ require (
 	golang.org/x/arch v0.3.0 // indirect
 	golang.org/x/crypto v0.8.0 // indirect
 	golang.org/x/net v0.9.0 // indirect
-	golang.org/x/sys v0.7.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- main.go
+++ main.go
@@ -1,45 +1,108 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"fmt"
 	"net/http"
+	"sync"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
 )
 
-func _string(context *gin.Context) {
-	context.String(http.StatusOK, "hello world!")
-}
-func _json(c *gin.Context) {
-	// 1. json in response to struct
-	type UserInfo struct {
-		UserName string `json:"user_name"`
-		Age int32 `json:"age"`
-		Password int32 `json:"-"` // ignore
+var (
+	upgrader = websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
 	}
-	user := UserInfo{"eland", 21, 12345}
-	c.JSON(200, user)
-	// 2. json in response to map
-	userMap := map[string]string{
-		"user_name":"eland",
-		"age":"22",
-	}
-	c.JSON(200, userMap)
-	// 3. in response to JSON directly
-	c.JSON(200, gin.H{"username":"eland", "age":23})
-}
-func _html(c *gin.Context) {
-	c.HTML(http.StatusOK, "index.html", gin.H{})
-}
+	msgChan = make(chan string) // 定义全局的消息通道
+
+	// 建立一个WebSocket连接的映射，并使用互斥锁进行保护
+	conns = struct {
+		sync.RWMutex
+		m map[*websocket.Conn]bool
+	}{m: make(map[*websocket.Conn]bool)}
+)
 
 func main() {
 	router := gin.Default()
-	router.LoadHTMLGlob("templates/index.html")
-	router.Static("/style", "/static/*")
-	router.GET("/index", _string)
-	router.GET("/json", _json)
-	router.GET("/html", _html)
-	//router.GET("/xml", _xml)
-	//router.GET("/yaml", _yaml)
-	http.ListenAndServe(":8080", router)
+	router.LoadHTMLGlob("templates/*.html")
+
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
+
+	router.GET("/login", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "login.html", nil)
+	})
+
+	router.POST("/login", func(c *gin.Context) {
+		username := c.PostForm("username")
+		password := c.PostForm("password")
+
+		if username == "" || password == "" {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Username and password are required"})
+			return
+		}
+
+		c.Redirect(http.StatusMovedPermanently, "/chat")
+	})
+
+	router.GET("/chat", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "chat.html", nil)
+	})
+
+	router.GET("/ws", func(c *gin.Context) {
+		wshandler(c.Writer, c.Request)
+	})
+
+	// 启动消息广播协程
+	go func() {
+		for {
+			msg := <-msgChan
+			broadcast(msg)
+		}
+	}()
+
+	router.Run(":8080")
 }
 
+func wshandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Printf("Failed to set websocket upgrade: %+v\n", err)
+		return
+	}
 
+	// 将新的WebSocket连接添加到映射中
+	conns.Lock()
+	conns.m[conn] = true
+	conns.Unlock()
+
+	defer func() {
+		// 当连接断开时，从映射中移除
+		conns.Lock()
+		delete(conns.m, conn)
+		conns.Unlock()
+	}()
+
+	for {
+		_, msg, err := conn.ReadMessage()
+		if err != nil {
+			break
+		}
+
+		// 将消息发送到全局的消息通道
+		msgChan <- string(msg)
+	}
+}
+
+func broadcast(msg string) {
+	conns.RLock()
+	defer conns.RUnlock()
+
+	for conn := range conns.m {
+		if err := conn.WriteMessage(websocket.TextMessage, []byte(msg)); err != nil {
+			fmt.Printf("Failed to broadcast message: %+v\n", err)
+		}
+	}
+}
